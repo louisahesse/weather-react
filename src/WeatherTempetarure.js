0a674d9c51c61479c54452032d15b66849efaab8
louisahesse <louisahesse09@gmail.com>
2023-02-13 13:43:33 +0000

    created new component

--- src/WeatherTempetarure.js
+++ src/WeatherTempetarure.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 
 export default function WeatherTemperature(props) {
-  return (
-    <div className="WeatherTemperature">
-      <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">°C | </span>{" "}
-    </div>
-  );
+  const [unit, setUnit] = useState("celcius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  function fahrenheit() {
+    return (props.celcius * 9) / 5 + 32;
+  }
+  if (unit === "celcius") {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          <a herf="/" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(fahrenheit)}</span>
+        <span className="unit">
+          <a herf="/" onClick={showCelsius}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  }
 }
