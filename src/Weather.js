30ae9a44118c5b17e1272f0d9775ae6966ab05d8
louisahesse <louisahesse09@gmail.com>
2023-02-13 14:23:03 +0000

    Created WeatherTemperature.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,134 +1,72 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import axios from "axios";
-import "./App.css";
+import React, { useState } from "react";
 import "./Weather.css";
-import "./WeatherInfo";
-import WeatherIcon from "./WeatherIcon";
-import CurrentLocation from "./CurrentLocation";
-import Search from "./Search";
-import DateUtil from "./FormattedDate";
-import Api from "./Api";
-import Forecast from "./Forecast";
-import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
+import WeatherInfo from "./WeatherInfo";
 
-export default class Weather extends Component {
-  static propTypes = {
-    city: PropTypes.string.isRequired,
-  };
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-  state = {
-    city: this.props.city,
-  };
-
-  componentWillMount() {
-    this.refresh(this.state.city);
-  }
-
-  refreshWeatherFromParams(params) {
-    let url = `${Api.url}/data/2.5/weather?appid=${Api.key}&units=metric&${params}`;
-    axios.get(url).then((response) => {
-      this.setState({
-        city: response.data.name,
-        weather: {
-          ready: true,
-          city: response.data.name,
-          date: new Date(response.data.dt * 1000),
-          description: response.data.weather[0].description,
-          icon: response.data.weather[0].icon,
-          humidity: Math.round(response.data.main.humidity) + "%",
-          temperature: Math.round(response.data.main.temp),
-          time: new DateUtil(new Date(response.data.dt * 1000)).dayTime(),
-          wind: Math.round(response.data.wind.speed) + "km/h",
-          iconUrl:
-            "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
-        },
-      });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coord: response.data.coord,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      date: new Date(response.data.dt * 1000),
+      icon: response.data.weather[0].icon,
+      iconUrl:
+        "http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png",
     });
   }
 
-  refreshWeatherFromLatitudeAndLongitude = (latitude, longitude) => {
-    this.refreshWeatherFromParams(`lat=${latitude}&lon=${longitude}`);
-  };
+  function search() {
+    const apiKey = "3323f2e20324d1953472069851a88a5a";
 
-  refresh = (city) => {
-    this.refreshWeatherFromParams(`q=${city}`);
-  };
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-  render() {
-    if (this.state.weather) {
-      return (
-        <div>
-          <div className="clearfix">
-            <Search refresh={this.refresh} />
-            <CurrentLocation
-              refresh={this.refreshWeatherFromLatitudeAndLongitude}
-            />
-          </div>
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
           <div className="row">
-            <div className="col-sm-6">
-              <div className="clearfix">
-                <div className="float-left weather-icon">
-                  <WeatherIcon iconName={this.state.weather.icon} />
-                </div>
-                <div className="weather-temp weather-temp--today">
-                  {this.state.weather.temperature}
-                </div>
-                <div className="weather-unit__text weather-unit__text--today">
-                  °C
-                </div>
-              </div>
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Search city.."
+                className="form-control"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className=" btn btn-primary"
+              />
             </div>
           </div>
-          <div className="weather-summary">
-            <div className="weather-summary-header">
-              <h1>{this.state.city}</h1>
-              <div className="weather-detail__text">
-                {this.state.weather.time}
-              </div>
-              <div className="weather-detail__text">
-                {this.state.weather.description}
-              </div>
-            </div>
-
-            <div className="row row-cols">
-              <div className="col">
-                <div className="card">
-                  <div className="card-body">
-                    <div className="weather-detail__text">
-                      Humidity: {this.state.weather.humidity}
-                    </div>
-                    <div className="weather-detail__text">
-                      Wind: {this.state.weather.wind}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="row row-cols">
-              <div className="col">
-                <div className="card">
-                  <div className="card-body">
-                    <div className="weather-detail__text">
-                      Sunrise: {this.state.weather.sunrise}
-                    </div>
-                    <div className="weather-detail__text">
-                      Sunset: {this.state.weather.sunset}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <Forecast city={this.state.city} />
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          App is loading, <em>please wait...</em>
-        </div>
-      );
-    }
+        </form>
+        <WeatherInfo data={weatherData} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
   }
 }
